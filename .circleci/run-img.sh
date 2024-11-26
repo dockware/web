628c348c2127bbad798e87c8a7e0647be82bfd40
Christian Dangl <boxblinkracer@gmx.net>
2024-11-26 20:25:13 +0100

    fix project name

--- .circleci/run-img.sh
+++ .circleci/run-img.sh
@@ -13,7 +13,7 @@ EOT
 
 cat body.json
 
-PROJECT_NAME=${IMAGE_NAME:-flex}
+PROJECT_NAME="flex"
 
 curl -X POST -d @body.json \
      -H "Content-Type: application/json" \
@@ -21,3 +21,5 @@ curl -X POST -d @body.json \
      "https://circleci.com/api/v2/project/github/dockware/${PROJECT_NAME}/pipeline"
 
 rm -rf body.json
+
+ 
