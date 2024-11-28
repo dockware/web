052e6d455d367fd171578d6c258072e0951472bb
Christian Dangl <boxblinkracer@gmx.net>
2024-11-28 20:45:17 +0100

    fix status command

--- src/assets/scripts/bin/status.sh
+++ src/assets/scripts/bin/status.sh
@@ -1,6 +1,8 @@
 echo apache2 status:
 service apache2 status
 
+CURRENT_PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;")
+
 echo ""
 echo ""
 echo "**********************************************"
@@ -9,6 +11,6 @@ echo "**********************************************"
 echo ""
 echo "PHP: $(php -v | grep cli)"
 echo ""
-service php${PHP_VERSION}-fpm status
+service php${CURRENT_PHP_VERSION}-fpm status
 echo ""
 php -v
