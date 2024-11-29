48248a2f5ea8d02d190b257c19162289037df1de
Christian Dangl <boxblinkracer@gmx.net>
2024-11-29 15:59:49 +0100

    add user as custom script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -49,25 +49,10 @@ if [ $RECOVERY_MODE = 0 ]; then
     sudo dpkg-reconfigure -f noninteractive tzdata
     echo "-----------------------------------------------------------"
 
-    # TODO custom scripts?
     # checks if a different username is set in ENV and create if its not existing yet
     if [ $SSH_USER != "not-set" ] && (! id -u "${SSH_USER}" >/dev/null 2>&1 ); then
         echo "DOCKWARE: creating additional SSH user...."
-        # create a custom ssh user for our provided settings
-        sudo adduser --disabled-password --uid 8888 --gecos "" --ingroup www-data $SSH_USER
-        sudo usermod -a -G sudo $SSH_USER
-        sudo usermod -m -d /var/www $SSH_USER | true
-        sudo echo "${SSH_USER}:${SSH_PWD}" | sudo chpasswd
-        sudo sed -i "s/${SSH_USER}:x:8888:33:/${SSH_USER}:x:33:33:/g" /etc/passwd
-        # add sudo without password
-        # write user to file cause we loos the var as we executing as root and get a new shell
-        sudo echo "${SSH_USER}" >> /tmp/user.name
-        sudo -u root sh -c 'echo "Defaults:$(cat /tmp/user.name) !requiretty" >> /etc/sudoers'
-        sudo rm -rf /tmp/user.name
-        # disable original ssh access
-        sudo usermod -s /bin/false dockware
-        # allow ssh in sshd_config
-        sudo sed -i "s/AllowUsers dockware/AllowUsers ${SSH_USER}/g" /etc/ssh/sshd_config
+        sh /var/www/scripts/bin/add_user.sh $SSH_USER $SSH_PWD
         echo "-----------------------------------------------------------"
     fi
 
