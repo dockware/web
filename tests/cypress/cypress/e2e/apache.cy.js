ddcdd6205826f4de1c280137a6cdc56b004e3331
Christian Dangl <boxblinkracer@gmx.net>
2024-11-29 12:38:20 +0100

    improve cypress tests

--- tests/cypress/cypress/e2e/apache.cy.js
+++ tests/cypress/cypress/e2e/apache.cy.js
@@ -4,7 +4,7 @@ describe('Apache Tests', () => {
         cy.script('clear-container.sh');
     });
 
-    it('Empty Apache Exists', () => {
+    it('Empty page without directory listing', () => {
 
         cy.visit('/', {failOnStatusCode: false});
 
@@ -16,15 +16,55 @@ describe('Apache Tests', () => {
 
 describe('PHP Tests', () => {
 
-    before(() => {
-        cy.script('setup-phpinfo.sh');
-    });
+    context('Apache renders PHP', () => {
+        before(() => {
+            cy.script('setup-phpinfo.sh');
+        });
 
-    it('PHP is rendered', () => {
+        it('PHP info is visible', () => {
+            cy.visit('/', {failOnStatusCode: false});
+            cy.contains('PHP Version');
+            cy.contains('PHP License');
+        })
+    })
 
-        cy.visit('/', {failOnStatusCode: false});
+    context('Xdebug enabled', () => {
+        before(() => {
+            cy.script('xdebug-on.sh');
+            cy.script('setup-phpinfo.sh');
+        });
 
-        cy.contains('PHP Version 8.4.1');
+        it('Xdebug data in PHP info', () => {
+            cy.visit('/', {failOnStatusCode: false});
+            cy.contains('xdebug.idekey');
+            cy.contains('PHPSTORM');
+        })
+    })
+
+    context('Xdebug disabled', () => {
+        before(() => {
+            cy.script('xdebug-off.sh');
+            cy.script('setup-phpinfo.sh');
+        });
+
+        it('No Xdebug data in PHP info', () => {
+            cy.visit('/', {failOnStatusCode: false});
+            cy.contains('xdebug.idekey').should('not.exist');
+            cy.contains('PHPSTORM').should('not.exist');
+        })
+    })
+
+    context('Switch PHP Version', () => {
+        before(() => {
+            cy.script('switch-php.sh 8.3');
+            cy.script('setup-phpinfo.sh');
+        });
+
+        it('Switch to PHP 8.3', () => {
+            cy.visit('/', {failOnStatusCode: false});
+            cy.contains('PHP Version 8.3.');
+        })
+
     })
 
 })
