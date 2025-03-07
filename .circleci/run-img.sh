a04004824738c749e231b85580437592d0837755
Christian Dangl <boxblinkracer@gmx.net>
2025-03-07 13:26:07 +0100

    fix pipe

--- .circleci/run-img.sh
+++ .circleci/run-img.sh
@@ -4,7 +4,7 @@ cat <<EOT >> body.json
 {
   "parameters":
       {
-          "imageName": "flex",
+          "imageName": "web",
           "imageTag" : "$2",
           "setLatest" : $3
       }
@@ -13,7 +13,7 @@ EOT
 
 cat body.json
 
-curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $1" https://circleci.com/api/v2/project/github/dockware/flex/pipeline
+curl -X POST -d @body.json -H "Content-Type: application/json" -H "Circle-Token: $1" https://circleci.com/api/v2/project/github/dockware/web/pipeline
 
 rm -rf body.json
 
