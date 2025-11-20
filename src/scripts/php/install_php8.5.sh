b7ac78895844bb5cbbba1138c6e32510e9bd83c9
Christian Dangl <boxblinkracer@gmx.net>
2025-11-20 13:24:08 +0100

    add more php8.5 modules

--- src/scripts/php/install_php8.5.sh
+++ src/scripts/php/install_php8.5.sh
@@ -1,15 +1,19 @@
 apt-get install -y php8.5-fpm \
                    php8.5-cli \
                    php8.5-gd \
+                   php8.5-iconv \
                    php8.5-intl \
                    php8.5-xml \
                    php8.5-mbstring \
+                   php8.5-pdo \
                    php8.5-mysql \
                    php8.5-curl \
                    php8.5-ldap \
                    php8.5-pgsql \
+                   php8.5-gettext \
                    php8.5-zip \
                    php8.5-soap \
+                   php8.5-apcu \
                    php8.5-bcmath
 
 # pecl install xdebug-beta
